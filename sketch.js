adb09caf8cc8466a84a87d0d24e56a2100f901ee
otawfik07 <74074787+otawfik07@users.noreply.github.com>
2020-12-12 00:35:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+{
+var car,wall;
+var speed, weight;
+var wall, thickness;
+var bullet, speed, weight;
+}
+function draw() {
+speed=random(55,90)
+weight=random(400,1500)
+car=createSprite(50,200,50,50);
+wall=createSprite(1500,200,60,height/2)
+wall=createSprite(1200, 200, thickness, height/2)
+thickness=random(22,83)
+car.velocityX = speed;
+var Deformation > 0.5 * Weight * Speed * Speed/22509
+if(deformation>180)
+{
+  car.shapecolor=color(255,0,0)
+if(deformation<180 && deformation > 100)
+   car.shapeColor(230,230,0)
+}
+if(deformation>180)
+car.shapeColor(230,230,0)
+
+
+
+  background(255,255,255);  
+  drawSprites();
+}
